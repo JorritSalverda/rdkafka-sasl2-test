6e6f4b1ae9fa7c6de6a28e250d969eeb5ae9ac52
Jorrit Salverda <jorrit.salverda@lightyear.one>
2022-02-20 08:12:01 +0100

    strip all lambda related crates and code; install openssl-dev

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 mod kafka_client;
 use kafka_client::{KafkaClient, KafkaClientConfig};
-use lambda_http::{
-    lambda_runtime::Error,
-};
 
-#[tokio::main]
-async fn main() -> Result<(), Error> {
+fn main() {
     let mut kafka_client =
         KafkaClient::new(KafkaClientConfig::from_env().expect("Failed to init KafkaClientConfig"));
 
     kafka_client.init_connection().expect("Failed to initialize connection to Kafka cluster");
-
-    Ok(())
 }
